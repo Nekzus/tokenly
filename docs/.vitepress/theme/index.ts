91194cada835949b16f7c948897204caa38933a7
Nekzus <Nekzus@users.noreply.github.com>
2025-01-22 03:47:05 +0000

    deploy: e9926884a9b8d3321bb7ce943c53e17ecda17588

--- docs/.vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
-
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    // Register global components if needed
-  }
-} 
